b61e4166d050f731a2a9dba23844d8dc458d4293
Aibek171 <129676487+Aibek171@users.noreply.github.com>
2023-04-13 17:09:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import cramervonmises_2samp
 
 chat_id = 1121374935 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.01
-    stat, p_value = ks_2samp(x, y)
-    if p_value < alpha:
+    res = cramervonmises_2samp(x, y)
+    if res.pvalue < alpha:
         return True
     else:
         return False
